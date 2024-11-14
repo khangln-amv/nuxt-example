eccc7f3a64a5d85820e5ba38b883ca65da7ab9f7
khangln-amv <nguyenkhang-le@am-bition.vn>
2024-11-14 17:01:29 +0700

    Add primevue and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,40 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4
-  }
+  },
+
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@primevue/nuxt-module'
+  ],
+
+  css: ['assets/styles/tailwind.css', 'assets/styles/base.css'],
+
+  postcss: {
+    plugins: {
+      'postcss-import': {},
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
+
+  // tailwindcss: {
+  //   config: {
+  //     theme: {
+  //       extend: {
+  //         // colors: { primary: colors.green }
+  //       }
+  //     },
+  //     plugins: [require('tailwindcss-primeui')]
+  //   }
+  // },
+
+  primevue: {
+    options: {
+      theme: 'none'
+    }
+  },
 })
